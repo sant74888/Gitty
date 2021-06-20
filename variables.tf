7785b159da5b75d9cf54aaf13fe01dbbf8c32bac
sant74888 <override007@gmail.com>
2021-06-20 17:59:49 +0000

    variables added

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+   default="AKIA3HZLWZB23AZTBFFD"
+}
+variable "secret_key" {
+   default="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"
+}
+variable "region" {
+   default="ap-south-1"	
+}
